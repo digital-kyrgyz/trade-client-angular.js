98ab9213d63de40b6d5c6607894dd46596163f1e
icoder-kg <melis.archabaev.kg@gmail.com>
2023-01-06 19:55:07 +0600

    Create admin/layout moduel

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutModule } from './layout/layout.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    LayoutModule,
+    CommonModule
+  ]
+})
+export class AdminModule { }
